137a551ffc863a169675da6e517473fae215d684
unknown <rafaelgsilveira@yahoo.com>
2021-01-21 16:36:24 -0300

    new logo and better Navbar CSS

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import styles from '../styles/Navbar.module.css'
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 
 
 const MenuItems = [
@@ -31,16 +35,23 @@ const MenuItems = [
 ]
 
 class Navbar extends Component{
+    state = {clicked: false}
+
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
     render() {
         return(
             <nav className={styles.navbarItems}>
-                <h1 className="navbar-logo">
-                    <img src="/logo_name_white.svg" alt="Ragasil" width="100" height="80"/>
+                <h1 className={styles.navbarLogo}>
+                    <img src="/logo_name_contorno.png" alt="Ragasil" width="200" height="40"/>
                 </h1>
-                <div className="menu-icon">
+                <div className='menu-icon' onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}> </i>
 
                 </div>
-                <ul >
+                <ul className={this.state.clicked ? 'nav-menu active' : styles.navMenu}>
                     {MenuItems.map((item, index)=>{
                         return(
                             <li key={index}>
