086284275abad6d1dc0e7353891f8ad9a858070d
unknown <rafaelgsilveira@yahoo.com>
2021-02-19 17:24:38 -0300

    Contact page initial layout

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,26 @@
+import React from "react";
+import styles from "../styles/Home.module.css"
 
 function Contact() {
     return (
-        <div>
-           Em construção
+        <div className="container">
+            <div>
+                <h2>Entre em Contato</h2>
+            </div>
+            <div className={styles.grid}>
+                <div className="column">
+                    <img src="/old_phone.jpg" alt="Phone" width="600" height="400" />
+
+                </div>
+                <div className="column">
+                    <ul>
+                        <li>Coffee</li>
+                        <li>Tea</li>
+                        <li>Milk</li>
+                    </ul>
+
+                </div>
+            </div>
         </div>
 
     )
